32c90496e2a2a6456b6ff019f674a46ac4449853
Zoaib <thisiszoaib@gmail.com>
2024-12-31 18:38:01 +0500

    chore: update version to 2.0.0 and enhance theme selection in app component with system mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { SampleDialogComponent } from './sample-dialog/sample-dialog.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,6 +11,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { TitleCasePipe } from '@angular/common';
+
+export interface AppTheme {
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -29,12 +37,25 @@ import { MatToolbar } from '@angular/material/toolbar';
     MatInputModule,
     MatCheckboxModule,
     MatExpansionModule,
+    MatButtonToggleModule,
+    MatMenuModule,
+    TitleCasePipe,
   ],
 })
 export class AppComponent {
   title = 'Angular material dark mode';
 
-  darkMode = signal(false);
+  appTheme = signal<'light' | 'dark' | 'system'>('system');
+
+  themes = signal<AppTheme[]>([
+    { name: 'light', icon: 'light_mode' },
+    { name: 'dark', icon: 'dark_mode' },
+    { name: 'system', icon: 'desktop_windows' },
+  ]);
+
+  selectedTheme = computed(() =>
+    this.themes().find((t) => t.name === this.appTheme())
+  );
 
   constructor(private dialog: MatDialog) {}
 
@@ -44,8 +65,9 @@ export class AppComponent {
     });
   }
 
-  applyDarkMode = effect(() => {
-    const darkMode = this.darkMode();
-    document.body.classList.toggle('darkMode', darkMode);
+  applyAppTheme = effect(() => {
+    const appTheme = this.appTheme();
+    const colorScheme = appTheme === 'system' ? 'light dark' : appTheme;
+    document.body.style.setProperty('color-scheme', colorScheme);
   });
 }
